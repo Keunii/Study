99cc900a7d3f34de19b88847e8f06d0359928d3b
Keunii <psk582@gmail.com>
2020-12-03 09:12:55 +0900

    201202

---
+++ C#_Socket_Example_Study/P2PMessenger_ShortMessage/01_201202_P2PMessenger_ShortMessage/SmsgRecvEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_201202_P2PMessenger_ShortMessage
+{
+    public delegate void SmsgRecvEventHandler(object sender, SmsgRecvEventArgs e);
+
+    public class SmsgRecvEventArgs:EventArgs
+    {
+        public IPEndPoint RemoteEndPoint
+        {
+            get;
+            private set;
+        }
+
+        public string Msg
+        {
+            get;
+            private set;
+        }
+
+        public string IPstr
+        {
+            get { return RemoteEndPoint.Address.ToString(); }
+        }
+
+        public int Port
+        {
+            get { return RemoteEndPoint.Port; }
+        }
+
+        public SmsgRecvEventArgs(IPEndPoint remote, string msg)
+        {
+            RemoteEndPoint = remote;
+            Msg = msg;
+        }
+    }
+}
