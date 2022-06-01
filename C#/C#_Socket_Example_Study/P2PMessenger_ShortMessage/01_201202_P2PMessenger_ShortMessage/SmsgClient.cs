35891729198dfa833f82295e6bfa2fd900b27682
Keunii <psk582@gmail.com>
2022-06-01 16:16:38 +0900

    reset

---
+++ C#/C#_Socket_Example_Study/P2PMessenger_ShortMessage/01_201202_P2PMessenger_ShortMessage/SmsgClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace _01_201202_P2PMessenger_ShortMessage
+{
+    public class SmsgClient
+    {
+        public static void SendMsg(string other_ip, int other_port, string text)
+        {
+            try
+            {
+                byte[] packet = new byte[1024];
+                MemoryStream ms = new MemoryStream(packet);
+                BinaryWriter bw = new BinaryWriter(ms);
+                bw.Write(text);
+                bw.Close();
+                ms.Close();
+
+                Socket sock = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(other_ip), other_port);
+                sock.Connect(ipep);
+                sock.Send(packet);
+                sock.Close();
+            }
+            catch { }
+        }
+
+        delegate void SendDele(string other_ip, int other_port, string text);
+        public static void SendMsgAsync(string other_ip, int other_port, string text)
+        {
+            SendDele dele = SendMsg;
+            dele.BeginInvoke(other_ip, other_port, text, null, null);
+        }
+    }
+}
---
+++ C#/C#_Socket_Example_Study/SocketProgram_Practice/02_201201_EcoServerApplication_DefineClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace _02_201201_EcoServerApplication_DefineClass
+{
+    class Program
+    {
+        static void Main(string[] args) // 서버를 라이브러리 상태로 남겨져 있다면, 서버를 끊거나 연결할 때 유용(?)
+        {
+            EchoServer es = new EchoServer("192.168.0.7", 10248);
+            es.ReceivedMsgEventHandler += Es_ReceivedMsgEventHandler;
+            es.AcceptedEventHandler += Es_AcceptedEventHandler;
+            es.ClosedEventHandler += Es_ClosedEventHandler;
+
+            if (es.Start() == false)
+            {
+                Console.WriteLine("서버 가동 실패");
+                return;
+            }
+            Console.ReadKey();
+        }
+
+        private static void Es_ClosedEventHandler(object sender, ClosedEventArgs e)
+        {
+            Console.WriteLine("{0}:{1}에서 연결을 닫음", e.IPStr, e.Prot);
+        }
+
+        private static void Es_ReceivedMsgEventHandler(object sender, ReceivedMsgEventArgs e)
+        {
+            Console.WriteLine("{0}:{1} → {2} ", e.IPStr, e.Prot, e.Msg);
+        }
+
+        private static void Es_AcceptedEventHandler(object sender, AcceptedEventArgs e)
+        {
+            Console.WriteLine("{0}:{1}에서 연결 했음", e.IPStr, e.Prot);
+        }
+    }
+}
